48cb7c4aa24bac389c34c811afce2d30e62992c8
Alex Martens <alex.smartens@gmail.com>
2018-11-13 20:02:23 -0500

    Added stack implementation

---
+++ stack.py
@@ -0,0 +1,73 @@
+# This stack.py implementation follows  idea from CLRS, Chapter 2
+
+class stack:
+
+    def __init__(self):
+        self.items = []
+        self.top = 0
+        self.debug = True
+
+    def is_empty(self):
+        return self.top == 0
+
+    def size(self):
+        return self.top
+
+    def peek(self):
+        if self.top > 0:
+            return self.items[self.top - 1]
+        else:
+            return None
+
+    def push(self, new_item):
+        self.items.append(new_item)
+        self.top += 1
+
+        if self.debug:
+            self.print()
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        else:
+            new_item = self.items[self.top - 1]
+            self.items = self.items[:self.top - 1] # the last list item is not included
+            self.top -= 1
+
+            if self.debug:
+                self.print()
+
+            return new_item
+
+    def print(self):
+        print(self.items)
+
+
+# Running simple example using the stack.py class
+myStack = stack()
+print("is_empty: " + str(myStack.is_empty()))
+print("top: " + str(myStack.top))
+
+myStack.push(15)
+myStack.push(6)
+myStack.push(2)
+myStack.push(9)
+print("is_empty: " + str(myStack.is_empty()))
+print("top: " + str(myStack.top))
+
+myStack.push(17)
+myStack.push(3)
+print("size " + str(myStack.size()))
+print("peek " + str(myStack.peek()))
+
+myStack.pop()
+print("top: " + str(myStack.top))
+myStack.pop()
+myStack.pop()
+myStack.pop()
+myStack.pop()
+print("top: " + str(myStack.top))
+myStack.pop()
+print("top: " + str(myStack.top))
+myStack.pop()
+myStack.pop()
