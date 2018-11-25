9884abdc57188a323b2ed94121bb81100ce48b94
Alex <alex.smartens@gmail.com>
2018-11-25 18:54:44 -0500

    Added tree_delete functional to AVL_tree

--- AVL_tree.py
+++ AVL_tree.py
@@ -51,18 +51,36 @@ class AVL_tree:
         else:
             height = node["h_right"]
 
+        # Initial check whether rebalancing required
         p = node["p"]
+        if abs(node["h_left"] - node["h_right"]) == 2:
+            if p is not None:
+                if p["left"]["key"] == node["key"]:
+                    branch = "left"
+                else:
+                    branch = "right"
 
+            node = self.AVL_rebalance(node)
+            height = max(node["h_left"], node["h_right"])
+
+            if p is None:
+                self.root = node
+            else:
+                p[branch] = node
+
+        # Follow node's parents until the root is reached
         while p is not None:
 
             if p["left"] is not None and node["key"] == p["left"]["key"]:
                 height += 1
-                if p["h_left"] < height:
-                    p["h_left"] = height
+                p["h_left"] = height
+                if p["h_right"] > height:
+                    height = p["h_right"]
             else:
                 height += 1
-                if p["h_right"] < height:
-                    p["h_right"] = height
+                p["h_right"] = height
+                if p["h_left"] > height:
+                    height = p["h_left"]
 
             node = p
             p = p["p"]
@@ -111,7 +129,6 @@ class AVL_tree:
         # Relink pointer pairs
         new_node = node["left"]
         new_node["p"] = node["p"]
-
 
         node["left"] = new_node["right"]
         if node["left"] is not None:
@@ -204,50 +221,75 @@ class AVL_tree:
         return p
 
 
-    # def transplant(self, u, v):
-    #     print("---transplant")
-    #     if u["p"] is None:
-    #         print("---+ 1 transplant")
-    #         self.root = v
-    #     elif u["p"]["left"] is not None and u["key"] == u["p"]["left"]["key"]:
-    #         print("---+ 2 transplant")
-    #         u["p"]["left"] = v
-    #     else:
-    #         print("---+ 3 transplant")
-    #         u["p"]["right"] = v
-    #     if v is not None:
-    #         print("---+ 4 transplant")
-    #         v["p"] = u["p"]
-    #
-    #
-    # def tree_delete(self, k):
-    #     z = self.tree_search(k)
-    #     if z is None:
-    #         return z
-    #     if z["left"] is None:
-    #         balance_point = z["right"]
-    #         self.transplant(z, z["right"])
-    #     elif z["right"] is None:
-    #         balance_point = z["left"]
-    #         self.transplant(z, z["left"])
-    #     else:
-    #         y = self.tree_minimum(z["right"])
-    #         if y["p"]["key"] != z["key"]:
-    #             balance_point = y["p"]
-    #             self.transplant(y, y["right"])
-    #             y["right"] = z["right"]
-    #             if y["right"] is not None:
-    #                 y["right"]["p"] = y
-    #         else:
-    #             balance_point = y
-    #         self.transplant(z, y)
-    #         y["left"] = z["left"]
-    #         y["left"]["p"] = y
-    #     self.AVL_update_balance(balance_point)
+    def transplant(self, u, v):
+        if u["p"] is None:
+            self.root = v
+        elif u["p"]["left"] is not None and u["key"] == u["p"]["left"]["key"]:
+            u["p"]["left"] = v
+        else:
+            u["p"]["right"] = v
+        if v is not None:
+            v["p"] = u["p"]
 
 
-# Running simple examples
+    def tree_delete(self, k):
+        z = self.tree_search(k)
+        if z is None:
+            return z
 
+        # Parameter initialization
+        extra_balance_point = None
+
+        if z["left"] is None:
+
+            if z["right"] is not None:
+                balance_point = z["right"]
+            else:
+                balance_point = z["p"]
+
+            self.transplant(z, z["right"])
+        elif z["right"] is None:
+
+            balance_point = z["left"]
+
+            self.transplant(z, z["left"])
+        else:
+            y = self.tree_minimum(z["right"])
+            if y["p"]["key"] != z["key"]:
+
+                extra_balance_point = y
+                balance_point = y["p"]
+
+                self.transplant(y, y["right"])
+                y["right"] = z["right"]
+                if y["right"] is not None:
+                    y["right"]["p"] = y
+            else:
+
+                balance_point = y
+
+            self.transplant(z, y)
+            y["left"] = z["left"]
+            y["left"]["p"] = y
+
+        # Update balance factors (height) at directly affected node/s by deletion
+        for node in [balance_point, extra_balance_point]:
+            if node is not None:
+                # Left child check
+                if node["left"] is not None:
+                    node["h_left"] = max(node["left"]["h_left"], node["left"]["h_right"]) + 1
+                else:
+                    node["h_left"] = 0
+                # Right child check
+                if node["right"] is not None:
+                    node["h_right"] = max(node["right"]["h_left"], node["right"]["h_right"]) + 1
+                else:
+                    node["h_right"] = 0
+        # Update balance factors (height) on the branch affected by deletion and rebalance if required
+        self.AVL_update_balance(balance_point)
+
+
+# Running simple example
 print("Right rotation example: ")
 my_tree = AVL_tree()
 my_tree.tree_insert(10)
@@ -316,6 +358,28 @@ my_tree.tree_insert(63)
 my_tree.tree_insert(50)
 my_tree.tree_insert(62)
 my_tree.tree_insert(51)
+print("my_tree.root: ")
+print(my_tree.root)
+print("my_tree.root[left]")
+print(my_tree.root["left"])
+print("my_tree.root[right]")
+print(my_tree.root["right"])
+tree_visualize(my_tree.root)
+
+# # Delete example 1
+# my_tree.tree_delete(43)
+# my_tree.tree_delete(62)
+# my_tree.tree_delete(63)
+# my_tree.tree_delete(8)
+# my_tree.tree_delete(9)
+
+# my_tree.tree_delete(6)
+# my_tree.tree_delete(50)
+
+# Delete example 2
+my_tree.tree_delete(50)
+
+
 print("my_tree.root: ")
 print(my_tree.root)
 print("my_tree.root[left]")
